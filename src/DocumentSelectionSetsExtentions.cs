ef0499a26f58cda1d831b967f28026d54d101eee
Pedram Elmi <pedram.elmi@gmail.com>
2022-05-07 22:46:45 +0430

    Added New Method for SelectionSets

---
+++ src/DocumentSelectionSetsExtentions.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.DocumentParts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedramElmi.Navisworks.Toolkit
+{
+    public static class DocumentSelectionSetsExtentions
+    {
+        /// <summary>
+        /// Returns all SelectionSets inside of the Document
+        /// </summary>
+        /// <param name="documentSelectionSets"></param>
+        /// <returns></returns>
+        public static HashSet<SelectionSet> GetSelectionSets(this DocumentSelectionSets documentSelectionSets)
+        {
+            return documentSelectionSets.RootItem.GetSelectionSets();
+        }
+    }
+}
---
+++ src/FolderItemExtentions.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Navisworks.Api;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PedramElmi.Navisworks.Toolkit
+{
+    public static class FolderItemExtentions
+    {
+        /// <summary>
+        /// Returns the SelectionSets s inside of the <see cref="FolderItem"/> object
+        /// </summary>
+        /// <param name="folderItem"></param>
+        /// <returns></returns>
+        public static HashSet<SelectionSet> GetSelectionSets(this FolderItem folderItem)
+        {
+            // get this item children's selection set
+            var thisSelectionSets = folderItem.Children.Where(child => child is SelectionSet).Select(child => child as SelectionSet);
+
+            var selectionSets = folderItem.Children.Where(child => child is FolderItem).Select(child => child as FolderItem).Select(child => child.GetSelectionSets()).Append(thisSelectionSets);
+
+            // intersect
+            return selectionSets
+                .Skip(1)
+                .Aggregate(
+                new HashSet<SelectionSet>(selectionSets.First()),
+                (h, e) => { h.IntersectWith(e); return h; });
+        }
+    }
+}
